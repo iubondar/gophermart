a84451cc2b7cfaff8e34da7e9e9eeedaf9716f4f
Ivan Bondar <iubondar@gmail.com>
2025-04-05 12:43:55 +0300

    Add gzip compression support

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/iubondar/gophermart/internal/compress"
 	"github.com/iubondar/gophermart/internal/handler"
 	"github.com/iubondar/gophermart/internal/logging"
 	"github.com/iubondar/gophermart/internal/storage"
@@ -17,7 +18,7 @@ func NewRouter(storage *storage.Storage) (chi.Router, error) {
 	balanceHandler := handler.NewBalanceHandler(storage)
 
 	router := chi.NewRouter()
-	router.Use(logging.WithLogging)
+	router.Use(logging.WithLogging, compress.WithGzipCompression)
 	router.Post("/api/user/register", registerHandler.Register)
 	router.Post("/api/user/login", loginHandler.Login)
 	router.Post("/api/user/orders", registerOrderHandler.RegisterOrder)
